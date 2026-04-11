6f88b60178e44d9385bd96597c96a6a85a0d86ac
JamGmilk <MiruJam@outlook.com>
2026-04-11 13:24:15 +0800

    Refractor RepoInfoDialog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,7 +60,6 @@ kotlin {
 dependencies {
     // Core & Lifecycle
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.foundation)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
@@ -68,17 +67,10 @@ dependencies {
 
     // Compose
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.material3)
-    implementation(libs.androidx.compose.ui.text)
-    implementation(libs.androidx.compose.runtime)
-    implementation(libs.androidx.compose.runtime.annotation)
     implementation(libs.androidx.compose.foundation)
-    implementation(libs.androidx.compose.foundation.layout)
+    implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.material.icons.extended)
-    implementation(libs.androidx.compose.animation)
+    implementation(libs.androidx.compose.foundation.layout)
 
     // Other AndroidX
     implementation(libs.androidx.documentfile)
@@ -86,7 +78,6 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.biometric)
     implementation(libs.androidx.datastore.preferences)
-    implementation(libs.androidx.activity)
 
     // Third-party Libraries
     implementation(libs.org.eclipse.jgit)
@@ -102,7 +93,6 @@ dependencies {
     implementation(libs.hilt.android)
     ksp(libs.hilt.compiler)
     implementation(libs.hilt.navigation.compose)
-    implementation(libs.hilt.lifecycle.viewmodel.compose)
 
     // Test Dependencies
     testImplementation(libs.junit)
