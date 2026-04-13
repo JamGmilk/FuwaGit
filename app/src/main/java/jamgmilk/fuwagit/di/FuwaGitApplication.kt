2a45d6e12cb01a0e29730f679742e93907f32b96
JamGmilk <Fish-Kokomi@outlook.com>
2026-04-13 14:25:25 +0800

    Add exception catching and logging

--- app/src/main/java/jamgmilk/fuwagit/di/FuwaGitApplication.kt
+++ app/src/main/java/jamgmilk/fuwagit/di/FuwaGitApplication.kt
@@ -2,6 +2,12 @@ package jamgmilk.fuwagit.di
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import jamgmilk.fuwagit.util.CrashLogManager
 
 @HiltAndroidApp
-class FuwaGitApplication : Application()
+class FuwaGitApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        CrashLogManager.init(this)
+    }
+}
